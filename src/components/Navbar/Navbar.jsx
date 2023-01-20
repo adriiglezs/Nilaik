ce6d77d8617f6a8cf400ee38aeb84eca983f65fd
Diego Pahua Silvan <88301232+D7EGPS@users.noreply.github.com>
2023-01-20 14:39:40 -0600

    add change prettier

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,41 @@
-import { Link } from "react-router-dom"
-import style from "./Navbar.module.css"
-import logo from "../../assets/logo.png"
-import cart from "../../assets/cart.svg"
-import user from "../../assets/user.svg"
-function Navbar(){
-return(
-<nav className={style.nav}>
-    <div className={style.logo}>
-    <Link to="/">
-        <img className={style.img} src={logo} alt="Nilaik logo de colores diversos"/>
-    </Link>
-    </div>
-    <div className={style.options}>
-    <Link to="/products">
-        <h2>Productos</h2>
-    </Link>
-    <Link to="/about">
-        <h2>Acerca de</h2>
-    </Link>
-    <Link to="/contact">
-        <h2>Contactanos</h2>
-    </Link>
-    <Link to="/login">
-        <img src={user} alt="usuario donde puedes logearte o comprobar si ya lo estas"/>
-    </Link>
-    <Link to="/Cart">
-        <img src={cart} alt="carro de compra para añadir tus productos" />
-    </Link>
-    </div>
-</nav>)
+import { Link } from "react-router-dom";
+import style from "./Navbar.module.css";
+import brand from "../../assets/brand.png";
+import cart from "../../assets/cart.svg";
+import user from "../../assets/user.svg";
+function Navbar() {
+  return (
+    <nav className={style.nav}>
+      <div className={style.logo}>
+        <Link to="/">
+          <img
+            className={style.img}
+            src={brand}
+            alt="Nilaik logo de colores diversos"
+          />
+        </Link>
+      </div>
+      <div className={style.options}>
+        <Link to="/products">
+          <h2>Productos</h2>
+        </Link>
+        <Link to="/about">
+          <h2>Acerca de</h2>
+        </Link>
+        <Link to="/contact">
+          <h2>Contactanos</h2>
+        </Link>
+        <Link to="/login">
+          <img
+            src={user}
+            alt="usuario donde puedes logearte o comprobar si ya lo estas"
+          />
+        </Link>
+        <Link to="/Cart">
+          <img src={cart} alt="carro de compra para añadir tus productos" />
+        </Link>
+      </div>
+    </nav>
+  );
 }
-export default Navbar
+export default Navbar;
