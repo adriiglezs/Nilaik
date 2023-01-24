f52bd6655887dbcd40a7a51582eb7b40bb9df02c
Diego Pahua Silvan <dpahuas@gmail.com>
2023-01-24 14:11:01 -0600

    add changes axel

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import AboutUs from "./pages/AboutUs/AboutUs"
 import Error from "./pages/Error/Error"
 import Home from "./pages/Home/Home"
 import Contact from "./pages/Contact/Contact"
-import Footer from "./components/Footer/Footer"
 import Login from "./pages/Login/Login.jsx"
 import Cart from "./pages/Cart/Cart"
+import Characters from "./pages/Characters/Characters"
+import JordanAdidas from "./pages/JordanAdidas/JordanAdidas"
+import NikePuma from "./pages/NikePuma/NikePuma"
 
 function App() {
   return (
@@ -18,6 +20,10 @@ function App() {
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path= "/cart" element={<Cart/>}/>
+        <Route path="/characters" element={<Characters/>}/>
+        <Route path="/jordan-adidas" element={<JordanAdidas/>}/>
+        <Route path="/nike-puma" element={<NikePuma/>}/>
+
         <Route path="*" element={<Error/>}/>
       </Routes>
     </>
---
+++ src/pages/JordanAdidas/JordanAdidas.jsx
@@ -0,0 +1,3 @@
+export default function JordanAdidas(){
+  return(<></>)
+}
