6a0f47c3a4430c879bb7dc26bb12aa22b113d360
Diego Pahua Silvan <dpahuas@gmail.com>
2023-01-24 16:30:31 -0600

    adding changes from social media about

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,38 +1,77 @@
-import Card from "../../components/Card/Card"
-import melanie from "../../assets/melanie-min.jpeg"
-import alan from "../../assets/alan-min.png"
-import axel from "../../assets/axel-min.jpeg"
-import victoria from "../../assets/victoria-min.jpg"
-import diego from "../../assets/diego-min.png"
-import adrian from "../../assets/adrian-min.jpg"
-import Footer from "../../components/Footer/Footer"
-import style from "./AboutUs.module.css"
+import Card from "../../components/Card/Card";
+import melanie from "../../assets/melanie-min.jpeg";
+import alan from "../../assets/alan-min.png";
+import axel from "../../assets/axel-min.jpeg";
+import victoria from "../../assets/victoria-min.jpg";
+import diego from "../../assets/diego-min.png";
+import jorge from "../../assets/jorge.jpg";
+import adrian from "../../assets/adrian-min.jpg";
+import Footer from "../../components/Footer/Footer";
+import style from "./AboutUs.module.css";
 
-export default function AboutUs(){
-    return(<>
-    <Card name = "Melanie Leal"
-    photo = {melanie}
-    biography = "Originaria del estado de Hidalgo, graduada de la Licenciatura en Historia del Arte."/>
+export default function AboutUs() {
+  return (
+    <>
+      <div className={style.background}>
+        <Card
+          name="Melanie Leal"
+          instagramUrl=""
+          githubUrl=""
+          linkedinUrl=""
+          photo={melanie}
+          biography="Originaria del estado de Hidalgo, graduada de la Licenciatura en Historia del Arte."
+        />
 
-    <Card name = "Alan Ramos"
-    photo = {alan}
-    biography = "Estudiante de ingenieria en informatica, originario de GDL. Me gusta la natacion, la mixologia y salir a conocer restaurantes bonitos. Rol Develop team."/>
+        <Card
+          name="Alan Ramos"
+          instagramUrl=""
+          githubUrl=""
+          linkedinUrl=""
+          photo={alan}
+          biography="Estudiante de ingenieria en informatica, originario de GDL. Me gusta la natacion, la mixologia y salir a conocer restaurantes bonitos. Rol Develop team."
+        />
 
-     <Card name = "Axel Mercado"
-    photo = {axel}
-    biography = "23 años, Vive en CDMX, Pasante de ingeniería en sistemas automotrices, apasionado por las motocicletas, videojuegos, ejercicio y programación. Rol en el proyecto Develop team."/>
+        <Card
+          name="Axel Mercado"
+          instagramUrl=""
+          githubUrl=""
+          linkedinUrl=""
+          photo={axel}
+          biography="23 años, Vive en CDMX, Pasante de ingeniería en sistemas automotrices, apasionado por las motocicletas, videojuegos, ejercicio y programación. Rol en el proyecto Develop team."
+        />
 
-     <Card name = "Victoria Bolaños"
-    photo = {victoria}
-    biography = "Amante de los libros, la danza, los idiomas, la tecnología y el diseño, licenciada en innovación de negocios y mercadotecnia, mujer en búsqueda de respuestas a las mil y un preguntas formuladas cada día. Parte del Development Team en el proyecto."/>
+        <Card
+          name="Victoria Bolaños"
+          instagramUrl=""
+          githubUrl=""
+          linkedinUrl=""
+          photo={victoria}
+          biography="Amante de los libros, la danza, los idiomas, la tecnología y el diseño, licenciada en innovación de negocios y mercadotecnia, mujer en búsqueda de respuestas a las mil y un preguntas formuladas cada día. Parte del Development Team en el proyecto."
+        />
+        <Card
+          name="Jorge Hernandez"
+          photo={jorge}
+          biography="Descripcion de jorge"
+        />
+        <Card
+          name="Diego Pahua"
+          instagramUrl="https://www.instagram.com/d7egps/"
+          githubUrl="https://github.com/d7egps"
+          linkedinUrl="https://www.linkedin.com/in/d7egps/"
+          photo={diego}
+          biography="Soy un miembro del equipo de desarrolladores con formación en Pedagogía y pasión por la arquitectura de software, con la necesidad de mantenerme actualizado y buscando oportunidades para crecer profesionalmente, con ganas de afrontar cualquier reto que se me presente."
+        />
 
-     <Card name = "Diego Pahua"
-    photo = {diego}
-    biography = "Soy un miembro del equipo de desarrolladores con formación en Pedagogía y pasión por la arquitectura de software, con la necesidad de mantenerme actualizado y buscando oportunidades para crecer profesionalmente, con ganas de afrontar cualquier reto que se me presente."/>
-
-     <Card name = "Adrián González"
-    photo = {adrian}
-    biography = "Originaria del estado de Hidalgo, graduada de la Licenciatura en Historia del Arte."/>
-    <Footer styleFoo={style.foo}/>
-    </>)
+        <Card
+          name="Adrián González"
+          instagramUrl=""
+          githubUrl=""
+          linkedinUrl=""
+          photo={adrian}
+          biography="Originaria del estado de Hidalgo, graduada de la Licenciatura en Historia del Arte."
+        />
+      </div>
+      <Footer styleFoo={style.foo} />
+    </>
+  );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
+import * as Yup from 'yup'
 export default function Login(){
     return(<div>
-        
     </div>)
 }
