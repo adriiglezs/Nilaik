a2c7a43284c577a6691ab8a7f18d106716b2f621
Jorge Hdz <johg.0792@gmail.com>
2023-01-28 19:19:39 -0600

    Añadí Unique Collection

---
+++ src/components/UniqueCollection/UniqueCollection.jsx
@@ -0,0 +1,25 @@
+import style from "./UniqueCollection.module.css";
+export default function UniqueCollection() {
+  return (
+    <section className={style.content}>
+      <img
+        src="https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        className={style.img}
+      />
+      <img
+        src="https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        className={style.img}
+      />
+      <div className={style.text}>
+        <h3>Colección única</h3>
+        <p>
+          Nuestro principal objetivo es la comodidad y que las personas que
+          utilicen nuestras prendas se vean y se sientan bien. Encuentra el
+          atuendo perfecto y desafía al mundo.
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/pages/NikePuma/NikePuma.jsx
+++ src/pages/NikePuma/NikePuma.jsx
@@ -1,3 +1,9 @@
-export default function NikePuma(){
-  return(<></>)
+import UniqueCollection from "../../components/UniqueCollection/UniqueCollection";
+
+export default function NikePuma() {
+  return (
+    <>
+      <UniqueCollection />
+    </>
+  );
 }
