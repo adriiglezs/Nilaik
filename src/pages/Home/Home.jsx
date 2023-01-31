1f043da1e5fb7ce57fda3fb8b7dbc011ccca6e72
mexavictoriab <victoryabvi@gmail.com>
2023-01-30 20:55:35 -0600

    Creación: Social media

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import SocialMedia from "../../components/Social-Media/SocialMedia"
+
+
+
+
 import Footer from "../../components/Footer/Footer"
 
 function Home(){
     return(<>
+    <SocialMedia/>
     <Footer/>
     </>)
 }
