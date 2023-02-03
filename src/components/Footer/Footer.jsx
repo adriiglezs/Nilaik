0eae4e623a4815f29e4117dd9ee59fa08be24325
adriiglezs <adriiglezs0497@gmail.com>
2023-02-02 23:33:28 -0600

    Complementación Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,54 @@
+import { Link } from "react-router-dom";
 import style from "./Footer.module.css";
+import nilaikFooter from "../../assets/nilaikFooter.png";
+
+
 export default function Footer({ styleFoo }) {
   return (
     <footer className={styleFoo ? styleFoo : style.content}>
-      <p className={style.text}>Direccion: Estado de México</p>
-      <p>Catalogo</p>
+      <div className={style.section}>
+        <Link to="/">
+          <img
+            className={style.img}
+            src={nilaikFooter}
+            alt="Nilaik logo de colores diversos" />
+        </Link>
+
+
+        <p className={style.address}>Direccion: Estado de México</p>
+        <p className={style.address}>Correo: niilaiik@hotmail.com</p>
+        <p className={style.address}></p>
+
+      </div>
+
+      <div className={style.catalogue}>
+        <h4>Catálogo</h4>
+        <ul>
+          <Link to="/characters" ><li>Personajes</li>
+          </Link>
+          <Link to="/nike-puma"><li>Nike</li></Link>
+          <Link to="/nike-puma"><li>Puma</li></Link>
+          <Link to="/jordan-adidas"><li>Jordan</li></Link>
+          <Link to="/jordan-adidas"><li>Adidas</li></Link>
+        </ul>
+      </div>
+
+      <div className={style.links}>
+        <h4>Links</h4>
+        <ul>
+          <Link to="/"><li>Inicio</li></Link>
+          <Link to="/about"><li>Acerca de</li></Link>
+          <Link to="/contact"><li>Contáctanos</li></Link>
+        </ul>
+      </div>
+
+      <div className={style.shipping}>
+        <h4>Envíos</h4>
+        <ul>
+          <Link to="/AskQuestions"><li>Preguntas frecuentes</li></Link>
+          <Link to="/AskQuestions"><li>Rastreo de envío</li></Link>
+        </ul>
+      </div>
     </footer>
   );
 }
--- src/pages/NikePuma/NikePuma.jsx
+++ src/pages/NikePuma/NikePuma.jsx
@@ -1,8 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import SocialMedia from "../../components/Social-Media/SocialMedia";
 import UniqueCollection from "../../components/UniqueCollection/UniqueCollection";
+import style from "./NikePuma.module.css";
 
 export default function NikePuma() {
   return (<>
     <UniqueCollection />
+    <SocialMedia />
+    <Footer styleFoo={style.foo} />
 
   </>)
 }
