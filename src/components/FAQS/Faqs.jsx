37c7ff00410eaf6beb13db527c56d0b514fd0acb
Axel Mercado <joaxel.mercado@gmail.com>
2023-02-06 14:30:29 -0600

    Casi al 90

--- src/components/FAQS/Faqs.jsx
+++ src/components/FAQS/Faqs.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import style from "../../pages/AskQuestions/AskQuestions.module.css"
 
-export default function FAQ ({faq,index,toggleFAQ}){
+export default function FAQ ({faq,index,toggleFAQ}) {
 return (
-  <div className={style.faq + (faq.open ? "open" : "")} key={index}
-   onClick ={()=>toggleFAQ(index)}>
+  <div 
+    className={style.faq + (faq.open ? 'open' : '')} 
+    key={index}
+    onClick ={()=>toggleFAQ(index)}
+  >
 
-    <div className={style.faq-question}>{faq.question}</div>
-    <div className={style.faq-answer}>{faq.answer}</div>
+    <div className={style.question}>
+        {faq.question}
+    </div>
+
+    <div className={style.answer}>
+        {faq.answer}
+    </div>
   </div>
 );
 
--- src/pages/AskQuestions/AskQuestions.jsx
+++ src/pages/AskQuestions/AskQuestions.jsx
@@ -1,28 +1,54 @@
 import style from "./AskQuestions.module.css";
 import React, {useState} from "react";
 import FAQ from '../../components/FAQS/Faqs';
+import Footer from '../../components/Footer/Footer'
 
 
 export default function AskQuestions() {
     const [faqs, setfaqs] = useState([
       {
-        question: "¿Loremo?",
+        question: "¿Cuál es el costo de envío?",
         answer: "loremp inmpsum",
         open: true
       },
       {
-        question: "¿Loremo?",
+        question: "¿Cuánto tardará la entrega en realizarse?",
         answer: "loremp inmpsum",
-        open: false
+        open: false,
       },
       {
-        question: "¿Loremo?",
+        question: "¿Cómo sabré que se ha confirmado mi pedido?",
         answer: "loremp inmpsum",
-        open: false
+        open: false,
+      },
+      {
+        question: "¿Qué pasa si no estoy en casa cuando se entrega mi pedido?",
+        answer: "loremp inmpsum",
+        open: false,
+      },
+      {
+        question: "¿Qué tipos de pago se aceptan?",
+        answer: "loremp inmpsum",
+        open: false,
+      },
+      {
+        question: "¿Cómo verifico el estado de mi pedido?",
+        answer: "loremp inmpsum",
+        open: false,
+      },
+      {
+        question: "¿Puedo cambiar o cancelar mi pedido una vez confirmado?",
+        answer: "loremp inmpsum",
+        open: false,
+      },
+      {
+        question: "¿Cómo devuelvo o cambio un artículo?",
+        answer: "loremp inmpsum",
+        open: false,
       },
     ]);
 
-    const toggleFAQ = index => (
+    const toggleFAQ = index => {
       setfaqs(faqs.map((faq,i)=> {
         if (i=== index){
           faq.open =!faq.open
@@ -31,18 +57,19 @@ export default function AskQuestions() {
         }
         return faq;
       }))
-    )
+    }
   
     return (
-    <>
-      <div className={style.FaqsContainer}>
-        <h2 className={style.faqsH2}> Preguntas Frecuentes </h2>
-        <div className={style.faqs}>
-            {faqs.map((faq,i)=> (
-                <FAQ faq={faq} index = {i} toggleFAQ = {toggleFAQ}/>
+      <>
+        <div className={style.FaqsContainer}>
+          <h2 className={style.faqsH2}> Preguntas Frecuentes </h2> {/*Header */}
+          <div className={style.faqs}>
+            {faqs.map((faq, i) => (
+              <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
             ))}
+          </div>
         </div>
-      </div>
-    </>
-  );
+        <Footer styleFoo={style.foo} />
+      </>
+    );
 }
