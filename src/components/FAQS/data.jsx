31456e7419e5b25e87a8d731bd4916d77375daba
Axel Mercado <joaxel.mercado@gmail.com>
2023-02-07 15:45:33 -0600

    Al 95

---
+++ src/components/FAQS/data.jsx
@@ -0,0 +1,51 @@
+const questions = [
+  {
+    id: 1,
+    question: "¿Cuál es el costo de envío?",
+    answer:
+      "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+  {
+    id: 2,
+    question: "¿Cuánto tardará la entrega en realizarse?",
+    answer:
+      "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+  },
+  {
+    id: 3,
+    question: "¿Cómo sabré que se ha confirmado mi pedido?",
+    answer:
+      "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    question: "¿Qué pasa si no estoy en casa cuando se entrega mi pedido?",
+    answer:
+      "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    question: "¿Qué tipos de pago se aceptan?",
+    answer:
+      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+  },
+  {
+    id: 6,
+    question: "¿Cómo verifico el estado de mi pedido?",
+    answer:
+      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+  },
+  {
+    id: 7,
+    question: "¿Puedo cambiar o cancelar mi pedido una vez confirmado?",
+    answer:
+      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+  },
+  {
+    id: 8,
+    question: "¿Cómo devuelvo o cambio un artículo?",
+    answer:
+      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+  },
+];
+export default questions;
--- src/pages/AskQuestions/AskQuestions.jsx
+++ src/pages/AskQuestions/AskQuestions.jsx
@@ -1,75 +1,27 @@
 import style from "./AskQuestions.module.css";
-import React, {useState} from "react";
-import FAQ from '../../components/FAQS/Faqs';
+import React, { useState } from "react";
+import data from "../../components/FAQS/data";
+import SingleQuestion from "../../components/FAQS/Question";
 import Footer from '../../components/Footer/Footer'
 
+const AskQuestions = () => {
+  const [questions, setQuestions] = useState(data);
 
-export default function AskQuestions() {
-    const [faqs, setfaqs] = useState([
-      {
-        question: "¿Cuál es el costo de envío?",
-        answer: "loremp inmpsum",
-        open: true
-      },
-      {
-        question: "¿Cuánto tardará la entrega en realizarse?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-      {
-        question: "¿Cómo sabré que se ha confirmado mi pedido?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-      {
-        question: "¿Qué pasa si no estoy en casa cuando se entrega mi pedido?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-      {
-        question: "¿Qué tipos de pago se aceptan?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-      {
-        question: "¿Cómo verifico el estado de mi pedido?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-      {
-        question: "¿Puedo cambiar o cancelar mi pedido una vez confirmado?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-      {
-        question: "¿Cómo devuelvo o cambio un artículo?",
-        answer: "loremp inmpsum",
-        open: false,
-      },
-    ]);
+  return (
+    <div>
+    <main>
+      <div className={style.container}>
+        <h3>Preguntas frecuentes</h3>
+        <section className={style.info}>
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+      <Footer styleFoo={style.foo} />
+      </div>
+  );
+};
 
-    const toggleFAQ = index => {
-      setfaqs(faqs.map((faq,i)=> {
-        if (i=== index){
-          faq.open =!faq.open
-        } else {
-          faq.open = false;
-        }
-        return faq;
-      }))
-    }
-  
-    return (
-      <>
-        <div className={style.FaqsContainer}>
-          <h2 className={style.faqsH2}> Preguntas Frecuentes </h2> {/*Header */}
-          <div className={style.faqs}>
-            {faqs.map((faq, i) => (
-              <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
-            ))}
-          </div>
-        </div>
-        <Footer styleFoo={style.foo} />
-      </>
-    );
-}
+export default AskQuestions;
