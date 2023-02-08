cb68b2d24642a15c40535ad23b7dfdab77d20e5a
Melanie Leal <melanie.lealb09@gmail.com>
2023-02-08 17:04:43 -0600

    revisar estilos Footer

--- src/pages/AskQuestions/AskQuestions.jsx
+++ src/pages/AskQuestions/AskQuestions.jsx
@@ -1,4 +1,8 @@
 import style from "./AskQuestions.module.css"
+import Footer from "../../components/Footer/Footer"
 export default function AskQuestions(){
-    return(<></>)
+    return(<>
+    
+    
+    </>)
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import Footer from "../../components/Footer/Footer"
 
 function Home(){
     return(<>
+    
     <SocialMedia/>
     <Footer/>
     </>)
