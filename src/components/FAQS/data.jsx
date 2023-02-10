7816a0389ea0df75c922537d0698de3f2995fec3
adriiglezs <adriiglezs0497@gmail.com>
2023-02-10 16:30:48 -0600

    Incorporación: respuestas de preguntas frecuentes

--- src/components/FAQS/data.jsx
+++ src/components/FAQS/data.jsx
@@ -3,49 +3,49 @@ const questions = [
     id: 1,
     question: "¿Cuál es el costo de envío?",
     answer:
-      "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+      "El costo de envío será de acuerdo con la zona y el peso del pedido, actualmente nuestro colaborador es la paquetería Castores, en la cual usted puede cotizar en el siguiente enlace https://www.castores.com.mx/ ",
   },
   {
     id: 2,
     question: "¿Cuánto tardará la entrega en realizarse?",
     answer:
-      "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+      "La entrega se realizará en un plazo mínimo de 24 horas posterior al momento en que usted realiza el pedido y se confirma su pago, dependiendo de la disponibilidad de las prendas que elija y de la opción de entrega de su preferencia. Presencial: según lo pactado con el cliente, puede concretarse la entrega para el día posterior a la confirmación del pedido o según la disponibilidad del cliente.  Envío: el pedido es enviado 5 días hábiles después de la confirmación del pedido, para poder cumplir con nuestros estándares de calidad.",
   },
   {
     id: 3,
     question: "¿Cómo sabré que se ha confirmado mi pedido?",
     answer:
-      "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+      "A través de la plataforma WhatsApp notificamos la confirmación de este; puede solicitar foto o videograbación de cómo se empaqueta su pedido (si desea recibir de forma presencial puede verificar sin compromiso). ",
   },
   {
     id: 4,
     question: "¿Qué pasa si no estoy en casa cuando se entrega mi pedido?",
     answer:
-      "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+      "La paquetería notifica y regresan el pedido a la sucursal que a su vez remite el producto a nuestras instalaciones, para que no presente ningún inconveniente con la guía que se le proporciona puede verifique el estatus y rastrear su pedido para poder tomar sus precauciones. ",
   },
   {
     id: 5,
     question: "¿Qué tipos de pago se aceptan?",
     answer:
-      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+      "Aceptamos pago en efectivo, transferencias bancarias y próximamente cargo a tarjetas de crédito y débito.",
   },
   {
     id: 6,
     question: "¿Cómo verifico el estado de mi pedido?",
     answer:
-      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+      "Existen dos momentos diferentes: antes del envío y posterior al mismo. Antes del pedido usted puede solicitar información directamente por medio de WhatsApp de cómo ha ido progresando su pedido. Al enviar le sugerimos seguir con el número de rastreo, el cual dará paso a observar su estatus. ",
   },
   {
     id: 7,
     question: "¿Puedo cambiar o cancelar mi pedido una vez confirmado?",
     answer:
-      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+      "Si el pedido no se ha enviado, no hay ningún problema de cambiar artículos, tallas o diseños al respecto, sin embargo, para una cancelación deberá evaluarse la situación de acuerdo con las políticas de la empresa. ",
   },
   {
     id: 8,
     question: "¿Cómo devuelvo o cambio un artículo?",
     answer:
-      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+      "Un punto importante es que los cambios solo se realizan por talla o defecto, pero no por diseño. En los cambios presenciales se hacen de forma directa. Si es por un envío realizado por paquetería y el cambio es por daño, corre por nuestra cuenta el envío del cambio, si es por talla corresponderá al cliente el costo adicional.",
   },
 ];
 export default questions;
