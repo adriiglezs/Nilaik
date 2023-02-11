71354725081d3814d66c9b6195c8137800d4c146
Melanie Leal <melanie.lealb09@gmail.com>
2023-02-11 15:29:46 -0600

    actualizar componente productos

---
+++ src/components/ProductDetailF/products.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import productsData from "./productsData";
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  const products = productsData.map(product => {
+    return (
+      <div key={product.id}>
+        <h3>
+          <Link to={`/products/${product.id}`}>{product.name}</Link>
+        </h3>
+        <p>Price: ${product.price}</p>
+        <hr />
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <h1>Products Page</h1>
+      {products}
+    </>
+  );
+};
+
+export default Products;
---
+++ src/components/ProductDetailF/productsData.jsx
@@ -0,0 +1,51 @@
+export default
+[
+  {
+    "Nombre": "Conjunto Jordan Rosa talla 2",
+    "Codigo": "CJRS2",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Jordan Rosa talla 4",
+    "Codigo": "CJRS4",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Jordan Rosa talla 6",
+    "Codigo": "CJRS6",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Jordan Rosa talla 8",
+    "Codigo": "CJRS8",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Jordan Rosa talla 10",
+    "Codigo": "CJRS10",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Jordan Rosa talla 12",
+    "Codigo": "CJRS12",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Nike Rosa talla 2",
+    "Codigo": "CNRS2",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  },
+  {
+    "Nombre": "Conjunto Nike Rosa talla 4",
+    "Codigo": "CNRS4",
+    "Menudeo": 175,
+    "Mayoreo": 160
+  }
+] 
