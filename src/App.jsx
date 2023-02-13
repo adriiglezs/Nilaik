6ce4d366273d66fe43e7b3c98f29b8eda996cfed
adriiglezs <adriiglezs0497@gmail.com>
2023-02-13 06:36:07 -0600

    prueba 4 errror 404

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} exact />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
