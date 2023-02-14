daad296e37bfe08150180209b4898e861b822ef2
adriiglezs <adriiglezs0497@gmail.com>
2023-02-13 18:01:11 -0600

    Prueba carousel automatico6

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { Carousel } from '@trendyol-js/react-carousel';
 import { adidasBicolor } from './data.js';
 import style from './Carousel.module.css';
@@ -18,6 +18,67 @@ function CarouselAdidasBi() {
       dots={false}
       slidesToShow={4}
       slidesToScroll={1}
+    >
+      {adidasBicolor.map(item => (
+        <div key={item.id} className={style.target}>
+          <div className={style.background}>
+            <img className={style.CardProduct} src={item.src} alt={item.name} />
+            <div className={style.name}>
+              <h1 className={style.nameL}>{item.name}</h1>
+            </div>
+            <div className={style.cod}>
+              <h2 className={style.codL}>{item.category}</h2>
+            </div>
+            <div className={style.cost}>
+              <h3 className={style.costL}>{item.price}</h3>
+            </div>
+            <div className={style.shopping}>
+              <img className={style.cards} src={cart} alt="Shopping cart"></img>
+            </div>
+          </div>
+        </div>
+      ))}
+    </Carousel>
+  </>);
+}
+
+export default CarouselAdidasBi; */
+
+////////////////////////
+import React from 'react';
+import { Carousel } from '@trendyol-js/react-carousel';
+import { adidasBicolor } from './data.js';
+import style from './Carousel.module.css';
+import cart from '../../assets/cart.svg';
+import slickArrow from '../../assets/user.svg';
+
+function CarouselAdidasBi() {
+  const PrevArrow = ({ onClick }) => (
+    <div className={style.prevArrow} onClick={onClick}>
+      <img src={slickArrow} alt="Previous Arrow" />
+    </div>
+  );
+
+  const NextArrow = ({ onClick }) => (
+    <div className={style.nextArrow} onClick={onClick}>
+      <img src={slickArrow} alt="Next Arrow" />
+    </div>
+  );
+
+  return (<>
+    <h1 className={style.encabezado}>Adidas Bicolor</h1>
+    <Carousel show={4.5} slide={2} transition={0.5} infinite
+      responsive={[{
+        breakpoint: 1024,
+        settings: { slidesToShow: 3 }
+      },
+      { breakpoint: 600, settings: { slidesToShow: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 1 } }]}
+      dots={false}
+      slidesToShow={4}
+      slidesToScroll={1}
+      prevArrow={<PrevArrow />}
+      nextArrow={<NextArrow />}
     >
       {adidasBicolor.map(item => (
         <div key={item.id} className={style.target}>
