e3d9d20255daabe43585a1aa4a77532415be2726
adriiglezs <adriiglezs0497@gmail.com>
2023-02-14 23:42:10 -0600

    correccion de ruta carrusel en characters

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,10 +1,10 @@
-import Carouselp from "../../components/CarouselP/Carouselp";
+
 import Footer from "../../components/Footer/Footer";
 import style from "./Characters.module.css";
 
 export default function Characters() {
   return (<>
-    <Carouselp />
+
     <Footer styleFoo={style.foo} />
   </>)
 }
