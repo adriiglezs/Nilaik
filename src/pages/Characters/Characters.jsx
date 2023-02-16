e2cf220f9dbc7727b1e971e6d3455d417f3f0dbc
adriiglezs <adriiglezs0497@gmail.com>
2023-02-15 23:38:04 -0600

    carrusel ui

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,10 +1,10 @@
-import CarouselAdidasBi from "../../components/Carousel/Carousel";
+import Example from "../../components/carouselJesus/carouselJesus";
 import Footer from "../../components/Footer/Footer";
 import style from "./Characters.module.css";
 
 export default function Characters() {
   return (<>
-    <CarouselAdidasBi />
+    <Example />
 
 
     <Footer styleFoo={style.foo} />
