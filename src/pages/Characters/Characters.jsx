2f46dbb4c6bcbe78c30799870c8d4e708e547bc1
adriiglezs <adriiglezs0497@gmail.com>
2023-02-15 23:16:28 -0600

    carrusel error

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,9 +1,14 @@
 
+import CarouselAdidasBi from "../../components/Carousel/Carousel";
+import Carouselp from "../../components/CarouselP/Carouselp";
 import Footer from "../../components/Footer/Footer";
 import style from "./Characters.module.css";
 
 export default function Characters() {
   return (<>
+    <CarouselAdidasBi />
+
+    <Carouselp />
 
     <Footer styleFoo={style.foo} />
   </>)
