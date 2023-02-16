86c0b1bea709f2e21b02183ab356de9aead83207
adriiglezs <adriiglezs0497@gmail.com>
2023-02-15 22:09:00 -0600

    incoporracion de target login

--- src/components/Login/CardLogin.jsx
+++ src/components/Login/CardLogin.jsx
@@ -5,6 +5,11 @@ export default function CardLogin() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [emailClicked, setEmailClicked] = useState(false);
+  const [passwordClicked, setPasswordClicked] = useState(false);
+
+  const handleEmailClick = () => setEmailClicked(true);
+  const handlePasswordClick = () => setPasswordClicked(true);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -16,6 +21,8 @@ export default function CardLogin() {
       console.log('Contraseña:', password);
       setEmail('');
       setPassword('');
+      setEmailClicked(false);
+      setPasswordClicked(false);
     }
   };
 
@@ -27,13 +34,31 @@ export default function CardLogin() {
         <form onSubmit={handleSubmit}>
           <div className={style.loginFormGroup}>
             <label htmlFor="email">Correo electrónico:</label>
-            <input required className={style.loginFormControl} name="email" id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            {email === '' && <span>Por favor ingrese su correo electrónico</span>}
+            <input
+              required
+              className={style.loginFormControl}
+              name="email"
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onClick={handleEmailClick}
+            />
+            {emailClicked && email === '' && <span>Por favor ingrese su correo electrónico</span>}
           </div>
           <div className={style.loginFormGroup}>
             <label htmlFor="password">Contraseña:</label>
-            <input required className={style.loginFormControl} name="password" id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            {password === '' && <span>Por favor ingrese su contraseña</span>}
+            <input
+              required
+              className={style.loginFormControl}
+              name="password"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onClick={handlePasswordClick}
+            />
+            {passwordClicked && password === '' && <span>Por favor ingrese su contraseña</span>}
           </div>
           <input type="submit" className={style.loginBtn} value="Ingresar" />
         </form>
