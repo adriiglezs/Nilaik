f34ccc6fcf0a9960d375cbb14ed9d04fe91465ac
adriiglezs <adriiglezs0497@gmail.com>
2023-02-16 22:28:49 -0600

    card product

--- src/pages/JordanAdidas/JordanAdidas.jsx
+++ src/pages/JordanAdidas/JordanAdidas.jsx
@@ -1,19 +1,15 @@
-import CardProduct from "../../components/CardProduct/CardProduct";
-import CardProductAdidas3 from "../../components/CardProduct/CardProductAdidas3";
-import CardProductJordan from "../../components/CardProduct/CardProductJordan";
-
 import Footer from "../../components/Footer/Footer";
 import SocialMedia from "../../components/Social-Media/SocialMedia";
 import UniqueCollection from "../../components/UniqueCollection/UniqueCollection";
 import style from "./JordanAdidas.module.css";
+import CardProductAdidas3 from "../../components/CardProduct/CardProductAdidas3"
 
 export default function JordanAdidas() {
   return (<>
     <div className={style.card21}>
-
-      <CardProductJordan />
-      <CardProduct />
       <CardProductAdidas3 />
+
+
     </div>
     <div className={style.card22}>
     </div>
